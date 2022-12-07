1f023898bbd79100fa35411d1769c4e9f9bba393
Chaman Deep Singh <chamandeepsingh019980@gmail.com>
2022-12-07 17:32:03 +0530

    Initial Commit

---
+++ new 1.js
@@ -0,0 +1,6 @@
+	function myFunction() {
+   document.getElementById("forlink").innerHTML = "Paragraph changed.";
+}
+	// function myFunction2() {
+   // document.getElementById("forlink").innerHTML = "Paragraph changed.";
+// }
